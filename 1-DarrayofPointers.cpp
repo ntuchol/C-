84caac15b6aa0b38e3ce9fbc09012d8c730f23f9
Natalia <ntuchol@bu.edu>
2025-09-06 23:02:39 -0500

    Update 1-DarrayofPointers.cpp

--- 1-DarrayofPointers.cpp
+++ 1-DarrayofPointers.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    char *strings[3]; // An array of 3 character pointers
-    size_t num_pointers = sizeof(strings) / sizeof(strings[0]); // Number of pointers
-    size_t size_of_one_pointer = sizeof(char*); // Size of a single pointer
+    char *strings[3]; 
+    size_t num_pointers = sizeof(strings) / sizeof(strings[0]); 
+    size_t size_of_one_pointer = sizeof(char*); 
 
-    printf("Number of pointers: %zu\n", num_pointers); // Output: 3
-    printf("Size of one pointer: %zu bytes\n", size_of_one_pointer); // Output: 8 bytes
-    printf("Total size of the array: %zu bytes\n", num_pointers * size_of_one_pointer); // Output: 24 bytes
+    printf("Number of pointers: %zu\n", num_pointers); 
+    printf("Size of one pointer: %zu bytes\n", size_of_one_pointer); 
+    printf("Total size of the array: %zu bytes\n", num_pointers * size_of_one_pointer); 
 
     return 0;
 }
