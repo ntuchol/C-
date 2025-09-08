bf7bcaaac0849e8cceec63facc6f84a3d681c392
Natalia <ntuchol@bu.edu>
2025-09-07 19:30:34 -0500

    Update Account_Class.cpp

--- Account_Class.cpp
+++ Account_Class.cpp
@@ -8,16 +8,13 @@ private:
     double balance;
 
 public:
-    // Constructor
     Account(const std::string& accNum, const std::string& holderName, double initialBalance)
         : accountNumber(accNum), accountHolderName(holderName), balance(initialBalance) {}
 
-    // Getters
     std::string getAccountNumber() const { return accountNumber; }
     std::string getAccountHolderName() const { return accountHolderName; }
     double getBalance() const { return balance; }
 
-    // Deposit method
     void deposit(double amount) {
         if (amount > 0) {
             balance += amount;
@@ -27,7 +24,6 @@ public:
         }
     }
 
-    // Withdraw method
     void withdraw(double amount) {
         if (amount > 0 && amount <= balance) {
             balance -= amount;
@@ -39,7 +35,6 @@ public:
         }
     }
 
-    // Display details method
     void displayAccountDetails() const {
         std::cout << "Account Number: " << accountNumber << std::endl;
         std::cout << "Account Holder: " << accountHolderName << std::endl;
