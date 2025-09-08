b332441ce89f5d642f1f8d18a7538f80309ee790
Natalia <ntuchol@bu.edu>
2025-09-07 19:20:53 -0500

    Update 2Darrayfromheap.cpp

--- 2Darrayfromheap.cpp
+++ 2Darrayfromheap.cpp
@@ -3,6 +3,5 @@ for(int i = 0 ; i < ROWS ; i++)
     dynamicArray[i] = new int[COLUMNS];
 
 double *myarray = new double[nrows * ncols]; 
-// Access element at [i][j]: myarray[i * ncols + j]
 #include <vector>
 std::vector<std::vector<int>> matrix(ROWS, std::vector<int>(COLUMNS));
