ffcc509558108fd45d216fc92aaaab0caed82c69
Natalia <ntuchol@bu.edu>
2025-09-07 19:27:45 -0500

    Update AarhusUniversity.cpp

--- AarhusUniversity.cpp
+++ AarhusUniversity.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
 int main() {
-  // The motto of Aarhus University is "Solidum petit in profundis".
-  // In English, this translates to "Seek a firm footing in the depths".
   std::cout << "The motto of Aarhus University is: Solidum petit in profundis" << std::endl;
   return 0;
 }
