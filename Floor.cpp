31c8dabace898599ee38b52952d75e229400a6bc
Natalia <ntuchol@bu.edu>
2025-09-12 12:13:00 -0500

    Update Floor.cpp

--- Floor.cpp
+++ Floor.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-#include <cmath> // Required for floor()
-
+#include <cmath> 
 int main() {
     double num1 = 2.7;
     double num2 = -2.7;
     double num3 = 5.0;
 
-    std::cout << "floor(" << num1 << ") = " << std::floor(num1) << std::endl; // Output: 2.0
-    std::cout << "floor(" << num2 << ") = " << std::floor(num2) << std::endl; // Output: -3.0
-    std::cout << "floor(" << num3 << ") = " << std::floor(num3) << std::endl; // Output: 5.0
+    std::cout << "floor(" << num1 << ") = " << std::floor(num1) << std::endl; 
+    std::cout << "floor(" << num2 << ") = " << std::floor(num2) << std::endl; 
+    std::cout << "floor(" << num3 << ") = " << std::floor(num3) << std::endl; 
 
     return 0;
 }
