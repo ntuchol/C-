ad2638d5f1390745209a3307ea6de80917689330
Natalia <ntuchol@bu.edu>
2025-09-11 23:32:25 -0500

    Update Bound_Linear_Search.cpp

--- Bound_Linear_Search.cpp
+++ Bound_Linear_Search.cpp
@@ -4,10 +4,10 @@
 int linearSearch(const std::vector<int>& arr, int target) {
     for (int i = 0; i < arr.size(); ++i) {
         if (arr[i] == target) {
-            return i; // Return the index if target is found
+            return i; 
         }
     }
-    return -1; // Return -1 if target is not found
+    return -1; 
 }
 
 int main() {
