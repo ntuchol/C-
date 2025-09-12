3b1e5e390a9f072e9dbed870338f4c38f9781006
Natalia <ntuchol@bu.edu>
2025-09-12 12:26:06 -0500

    Update Heap_Array.cpp

--- Heap_Array.cpp
+++ Heap_Array.cpp
@@ -5,19 +5,16 @@ int main() {
     std::cout << "Enter array size: ";
     std::cin >> size;
 
-    // Allocate an array of integers on the heap
     int* heapArray = new int[size];
 
-    // Initialize and access elements
     for (int i = 0; i < size; ++i) {
         heapArray[i] = i * 10;
         std::cout << heapArray[i] << " ";
     }
     std::cout << std::endl;
 
-    // Deallocate the memory when no longer needed
     delete[] heapArray;
-    heapArray = nullptr; // Good practice to set pointer to nullptr after deletion
+    heapArray = nullptr; 
 
     return 0;
 }
