e4203729147d1bf866c679961c37a17f2ef157e6
Natalia <ntuchol@bu.edu>
2025-09-11 23:27:28 -0500

    Update Bakery_Algorithm.cpp

--- Bakery_Algorithm.cpp
+++ Bakery_Algorithm.cpp
@@ -1,4 +1,3 @@
-// Shared data
 std::atomic<bool> choosing[NUM_THREADS];
 std::atomic<int> number[NUM_THREADS];
 
@@ -6,12 +5,12 @@ void lock(int process_id) {
     choosing[process_id] = true;
     int max_ticket = 0;
     for (int i = 0; i < NUM_THREADS; ++i) {
-        int current_ticket = number[i].load(); // Atomic read
+        int current_ticket = number[i].load(); 
         if (current_ticket > max_ticket) {
             max_ticket = current_ticket;
         }
     }
-    number[process_id].store(max_ticket + 1); // Atomic write
+    number[process_id].store(max_ticket + 1); 
     choosing[process_id] = false;
 
     for (int j = 0; j < NUM_THREADS; ++j) {
@@ -27,5 +26,5 @@ void lock(int process_id) {
 }
 
 void unlock(int process_id) {
-    number[process_id].store(0); // Atomic write
+    number[process_id].store(0);
 }
