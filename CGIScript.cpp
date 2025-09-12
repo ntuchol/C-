ab846566ba312b234a4e270a5a86db07dec9a209
Natalia <ntuchol@bu.edu>
2025-09-12 13:39:04 -0500

    Update CGIScript.cpp

--- CGIScript.cpp
+++ CGIScript.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
     int main() {
-        std::cout << "Content-type: text/html\n\n"; // Header and blank line
+        std::cout << "Content-type: text/html\n\n"; 
         std::cout << "<html><body><h1>Hello from C++ CGI!</h1></body></html>\n";
         return 0;
     }
