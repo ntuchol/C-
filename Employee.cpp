6bada33d3f844261565149ec71742b51bd6c4f52
Natalia <ntuchol@bu.edu>
2025-09-12 18:18:38 -0500

    Update Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -7,20 +7,16 @@ private:
     double salary;
 
 public:
-    // Constructor
     Employee(int id, std::string empName, double empSalary) {
         employeeID = id;
         name = empName;
         salary = empSalary;
     }
 
-    // Member functions
     void displayDetails() {
-        // Code to display employee details
     }
 
     double calculateYearlySalary() {
-        // Code to calculate yearly salary
         return salary * 12;
     }
 };
