d8fae80ac0869455d2b57c20b89776e2015e10bb
Natalia <ntuchol@bu.edu>
2025-09-11 23:30:34 -0500

    Update Binary_Full_Adder.cpp

--- Binary_Full_Adder.cpp
+++ Binary_Full_Adder.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 
-// Function to simulate a full adder
 void fullAdder(int A, int B, int Cin, int& Sum, int& Cout) {
-    Sum = Cin ^ (A ^ B); // XOR for sum calculation
-    Cout = (A & B) | (B & Cin) | (A & Cin); // AND and OR for carry-out calculation
+    Sum = Cin ^ (A ^ B); 
+    Cout = (A & B) | (B & Cin) | (A & Cin); 
 }
 
 int main() {
