7ac70372eff838da9f111525a2b749e5c1ca1a9e
Natalia <ntuchol@bu.edu>
2025-09-12 15:06:16 -0500

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -1,11 +1,8 @@
-// calculator.h
     class Calculator {
     public:
         int add(int a, int b);
-        // ... other methods
     };
 
-    // main.cpp (Driver Program)
     #include "calculator.h"
     #include <iostream>
 
@@ -13,16 +10,13 @@
         Calculator calc;
         int result = calc.add(5, 3);
         std::cout << "Result of addition: " << result << std::endl;
-        // ... test other functionalities
         return 0;
     }
-    // sensor_driver.h
-    class SensorDriver {
+
+class SensorDriver {
     public:
         SensorDriver();
         bool initialize();
         double readTemperature();
-        // ... other sensor-specific methods
     private:
-        // ... hardware interaction details
     };
