662e9eca6b47ca0c67702fa79e3e6f4ffbb4da4b
Natalia <ntuchol@bu.edu>
2025-09-12 15:27:44 -0500

    Update FilesonaDisk.cpp

--- FilesonaDisk.cpp
+++ FilesonaDisk.cpp
@@ -1,16 +1,12 @@
 #include <fstream>
-    // ...
-    ofstream outFile("example.txt"); // Opens "example.txt" for writing
+    ofstream outFile("example.txt"); 
     ifstream inFile("data.txt");
     if (!outFile.is_open()) {
-        // Handle error
     }
-    // Or:
     if (!inFile) {
-        // Handle error
     }
         outFile << "Hello, C++ File Handling!" << endl;
     string line;
-    getline(inFile, line); // Reads a line into 'line'
+    getline(inFile, line); 
     outFile.close();
     inFile.close();
