322881cdf1915357592eee9b6d5351d6e9517869
Natalia <ntuchol@bu.edu>
2025-09-12 13:23:33 -0500

    Update Button.cpp

--- Button.cpp
+++ Button.cpp
@@ -1,16 +1,13 @@
  #include <QtWidgets/QApplication>
-    #include <QtWidgets/QPushButton>
-    #include <QtWidgets/QMainWindow>
+ #include <QtWidgets/QPushButton>
+ #include <QtWidgets/QMainWindow>
 
     int main(int argc, char *argv[]) {
         QApplication a(argc, argv);
         QMainWindow window;
         QPushButton *button = new QPushButton("Click Me", &window);
-        // Position and size the button as needed
         button->setGeometry(50, 50, 100, 30); 
-        // Connect the button's clicked signal to a slot (function)
         QObject::connect(button, &QPushButton::clicked, [&]() {
-            // Code to execute when the button is clicked
             qDebug() << "Button clicked!";
         });
         window.show();
