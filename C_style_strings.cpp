41b52c9df06a84ad393f5e7ed4e00c0eb344d8f8
Natalia <ntuchol@bu.edu>
2025-09-12 13:40:13 -0500

    Update C_style_strings.cpp

--- C_style_strings.cpp
+++ C_style_strings.cpp
@@ -1,2 +1,2 @@
-char myString[] = "Hello"; // Compiler infers size and adds null terminator
-char anotherString[10] = "World"; // Must ensure enough space for string + null terminator
+char myString[] = "Hello"; 
+char anotherString[10] = "World"; 
