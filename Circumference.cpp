bfa831cdeda61731b9041d2b5594b5950a6ce016
Natalia <ntuchol@bu.edu>
2025-09-12 13:55:33 -0500

    Update Circumference.cpp

--- Circumference.cpp
+++ Circumference.cpp
@@ -9,7 +9,7 @@ int main() {
     std::cout << "Enter the radius of the circle: ";
     std::cin >> radius;
 
-    circumference = 2 * M_PI * radius; // M_PI is defined in <cmath>
+    circumference = 2 * M_PI * radius; 
 
     std::cout << std::fixed << std::setprecision(2); 
     std::cout << "The circumference of the circle is: " << circumference << std::endl;
