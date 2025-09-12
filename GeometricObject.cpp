2ed3a5ee9b4d0356a583aa254ed317deda33b2a2
Natalia <ntuchol@bu.edu>
2025-09-12 12:29:35 -0500

    Update GeometricObject.cpp

--- GeometricObject.cpp
+++ GeometricObject.cpp
@@ -4,14 +4,11 @@ private:
     double y;
 
 public:
-    // Constructor
     Point(double x_val, double y_val) : x(x_val), y(y_val) {}
 
-    // Getters
     double getX() const { return x; }
     double getY() const { return y; }
 
-    // Method to calculate distance from another point
     double distanceFrom(const Point& other) const {
         return std::sqrt(std::pow(x - other.x, 2) + std::pow(y - other.y, 2));
     }
