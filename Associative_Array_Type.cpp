deac405d747e0bba9238a0504b91ee9e0de2ca41
Natalia <ntuchol@bu.edu>
2025-09-11 23:12:46 -0500

    Update Associative_Array_Type.cpp

--- Associative_Array_Type.cpp
+++ Associative_Array_Type.cpp
@@ -1,23 +1,23 @@
 #include <map>
 #include <string>
 
-    std::map<std::string, int> ages; // Key: string, Value: int
+    std::map<std::string, int> ages; 
     ages["Alice"] = 30;
     ages["Bob"] = 25;
-    int alice_age = ages["Alice"]; // Accessing value by key
+    int alice_age = ages["Alice"]; 
 
 
 #include <unordered_map>
 #include <string>
 
-    std::unordered_map<std::string, double> prices; // Key: string, Value: double
+    std::unordered_map<std::string, double> prices; 
     prices["Apple"] = 1.25;
     prices["Banana"] = 0.75;
-    double apple_price = prices["Apple"]; // Accessing value by key
+    double apple_price = prices["Apple"]; 
 
 #include <map>
 #include <string>
 
-    std::multimap<std::string, std::string> synonyms; // Key: string, Value: string
+    std::multimap<std::string, std::string> synonyms; 
     synonyms.insert({"happy", "joyful"});
     synonyms.insert({"happy", "content"});
