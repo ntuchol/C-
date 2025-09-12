e468b1f92bf9b09693310728883e9e1af1ca97da
Natalia <ntuchol@bu.edu>
2025-09-12 18:35:16 -0500

    Update LoudNoiseTravelsEarthsCircumference.cpp

--- LoudNoiseTravelsEarthsCircumference.cpp
+++ LoudNoiseTravelsEarthsCircumference.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 int main() {
-    double circumference_meters = 40075000.0; // Earth's circumference in meters
-    double speed_of_sound_mps = 343.0;     // Speed of sound in meters per second
+    double circumference_meters = 40075000.0; 
+    double speed_of_sound_mps = 343.0;   
 
     double time_in_seconds = circumference_meters / speed_of_sound_mps;
-    double time_in_hours = time_in_seconds / 3600.0; // 3600 seconds in an hour
+    double time_in_hours = time_in_seconds / 3600.0; 
 
     std::cout << "Time for sound to travel around the Earth: " << time_in_hours << " hours" << std::endl;
 
