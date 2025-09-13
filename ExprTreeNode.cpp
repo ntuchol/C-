39791ba83a968e910f4358f4cdbcd3f1ebb4ce3a
Natalia <ntuchol@bu.edu>
2025-09-12 19:17:16 -0500

    Update ExprTreeNode.cpp

--- ExprTreeNode.cpp
+++ ExprTreeNode.cpp
@@ -1,19 +1,15 @@
 class ExprTreeNode {
 public:
-    // Constructor for operand node
     ExprTreeNode(double val); 
 
-    // Constructor for operator node
     ExprTreeNode(char op, ExprTreeNode* left, ExprTreeNode* right); 
 
-    // Member variables
-    bool is_operand; // True if operand, false if operator
-    double value;    // Stores value if is_operand is true
-    char operation;  // Stores operator if is_operand is false
+    bool is_operand; 
+    double value;    
+    char operation;  
     ExprTreeNode* left;
     ExprTreeNode* right;
 
-    // Methods for evaluation, printing, etc.
     double evaluate(); 
     void print_inorder();
 };
