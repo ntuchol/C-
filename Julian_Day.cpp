b455d082c467aa6820b90377476b8dc5cb284fcb
Natalia <ntuchol@bu.edu>
2025-09-12 20:29:46 -0500

    Update Julian_Day.cpp

--- Julian_Day.cpp
+++ Julian_Day.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 
 long calculateJulianDay(int year, int month, int day) {
-    // Adjust month and year for January and February
     if (month <= 2) {
         year -= 1;
         month += 12;
     }
 
-    // Calculate A for leap year adjustments
     long A = year / 100;
     long B = 2 - A + (A / 4);
 
-    // Fliegel & Van Flandern equation for Julian Day Number
     long JD = 365 * year + (year / 4) + (306 * (month + 1) / 10) + day + 1720299 + B;
 
     return JD;
