db0fa99a96460d99d6125ba9f5b1206364406f02
Natalia <ntuchol@bu.edu>
2025-09-12 20:35:53 -0500

    Update LongNumber.cpp

--- LongNumber.cpp
+++ LongNumber.cpp
@@ -1,2 +1,2 @@
-    long large_integer = 2147483647L; // The 'L' suffix denotes a long literal
-long long very_large_integer = 9223372036854775807LL; // The 'LL' suffix denotes a long long literal
+    long large_integer = 2147483647L; 
+long long very_large_integer = 9223372036854775807LL; 
