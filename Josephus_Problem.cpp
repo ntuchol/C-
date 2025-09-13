7ece38f5072bfc422283687961a3ac44981116cb
Natalia <ntuchol@bu.edu>
2025-09-12 20:29:20 -0500

    Update Josephus_Problem.cpp

--- Josephus_Problem.cpp
+++ Josephus_Problem.cpp
@@ -2,9 +2,6 @@ int josephus(int n, int k) {
         if (n == 1) {
             return 1;
         } else {
-            // The position of the survivor in a circle of n-1 people,
-            // shifted by k-1 and then taken modulo n to wrap around,
-            // and finally adding 1 to convert from 0-indexed to 1-indexed.
             return ((josephus(n - 1, k) + k - 1) % n) + 1;
         }
     }
@@ -13,7 +10,7 @@ int josephus(int n, int k) {
 
     int josephus_iterative(int n, int k) {
         std::vector<int> people(n);
-        std::iota(people.begin(), people.end(), 1); // Fill with 1 to n
+        std::iota(people.begin(), people.end(), 1);
 
         int current_index = 0;
         while (people.size() > 1) {
