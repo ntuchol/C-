47f9df443c02fc12567dda85e6977fbde162c63e
Natalia <ntuchol@bu.edu>
2025-09-12 19:18:03 -0500

    Update FSM.cpp

--- FSM.cpp
+++ FSM.cpp
@@ -8,25 +8,20 @@
             case State::Idle:
                 if (event == Event::Start) {
                     currentState = State::Running;
-                    // Perform start actions
                 }
                 break;
             case State::Running:
                 if (event == Event::Stop) {
                     currentState = State::Idle;
-                    // Perform stop actions
                 } else if (event == Event::Pause) {
                     currentState = State::Paused;
-                    // Perform pause actions
                 }
                 break;
             case State::Paused:
                 if (event == Event::Resume) {
                     currentState = State::Running;
-                    // Perform resume actions
                 } else if (event == Event::Stop) {
                     currentState = State::Idle;
-                    // Perform stop actions
                 }
                 break;
         }
