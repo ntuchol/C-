dd7e6b31413f30002ebadf96a8ca98740a1965ab
Natalia <ntuchol@bu.edu>
2025-09-13 19:13:21 -0500

    Update MaxNode.cpp

--- MaxNode.cpp
+++ MaxNode.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <algorithm> // Required for std::max
+#include <algorithm> 
 
     int main() {
         int a = 10;
         int b = 20;
-        int max_val = std::max(a, b); // Finds the maximum of two values
+        int max_val = std::max(a, b); 
         std::cout << "Max of a and b: " << max_val << std::endl;
 
-        // Using with an initializer list (C++11 and later)
         int max_list_val = std::max({5, 12, 3, 8, 15});
         std::cout << "Max in list: " << max_list_val << std::endl;
 
