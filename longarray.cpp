133d6df7276c867ea91372cb3c296503a1264d9e
Natalia <ntuchol@bu.edu>
2025-09-13 23:14:07 -0500

    Update longarray.cpp

--- longarray.cpp
+++ longarray.cpp
@@ -1,13 +1,8 @@
-// Array of 'long' integers
 long numbers[10]; 
 
-// Array of 'long long' integers for even larger values
 long long big_numbers[5]; 
 
-// Dynamically allocated large array of 'long long'
 long long* very_large_array = new long long[1000000000]; 
 
-// ... use the array ...
 
-// Deallocate memory when no longer needed
 delete[] very_large_array;
