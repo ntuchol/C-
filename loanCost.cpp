fee958ecbd33829a90c32939017347f7dfa2dd5f
Natalia <ntuchol@bu.edu>
2025-09-13 23:13:45 -0500

    Update loanCost.cpp

--- loanCost.cpp
+++ loanCost.cpp
@@ -1,9 +1,7 @@
-    double monthlyInterestRate = (annualInterestRate / 100) / 12;
-    int numberOfPayments = loanDurationYears * 12;
+double monthlyInterestRate = (annualInterestRate / 100) / 12;
+int numberOfPayments = loanDurationYears * 12;
 M = P * [R * (1 + R)^N] / [(1 + R)^N - 1]
-#include <cmath> // For pow() function
-
-// ... (previous code)
+#include <cmath> 
 
 double monthlyPayment = (loanAmount * monthlyInterestRate * pow(1 + monthlyInterestRate, numberOfPayments)) / (pow(1 + monthlyInterestRate, numberOfPayments) - 1);
 double totalAmountPaidBack = monthlyPayment * numberOfPayments;
