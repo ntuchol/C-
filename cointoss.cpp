1f1d8704608b6a82381de001072ca114540b997e
Natalia <ntuchol@bu.edu>
2025-09-14 14:27:18 -0500

    Update cointoss.cpp

--- cointoss.cpp
+++ cointoss.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-#include <cstdlib> // For rand() and srand()
-#include <ctime>   // For time()
+#include <cstdlib> 
+#include <ctime>   
 
-// Function to simulate a single coin toss
 std::string coinToss() {
-    // Generate a random number (0 or 1)
     int result = rand() % 2; 
 
     if (result == 0) {
@@ -15,7 +13,6 @@ std::string coinToss() {
 }
 
 int main() {
-    // Seed the random number generator once
     srand(static_cast<unsigned int>(time(NULL))); 
 
     int numTosses;
