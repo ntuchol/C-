786ffd39dfa13cde5aa76c1181f16a32a9b027ec
Natalia <ntuchol@bu.edu>
2025-09-14 00:58:33 -0500

    Update WString.cpp

--- WString.cpp
+++ WString.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main() {
-    std::wstring wstr = L"Hello, world! 🌍"; // Initialize with a wide string literal
-    std::wcout << wstr << std::endl; // Print using wide character output stream
+    std::wstring wstr = L"Hello, world! 🌍"; 
+    std::wcout << wstr << std::endl; 
     return 0;
 }
