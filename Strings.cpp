94b656a0b2a568cef46d28b76d01246f56402ee7
Natalia <ntuchol@bu.edu>
2025-09-14 16:16:53 -0500

    Update Strings.cpp

--- Strings.cpp
+++ Strings.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
-#include <string> // Required for std::string
-
+#include <string> 
 int main() {
-    std::string str1; // Declare the first string variable
-    std::string str2; // Declare the second string variable
-
-    // Prompt the user to enter the first string
+    std::string str1; 
+    std::string str2; 
+    
     std::cout << "Enter the first string: ";
-    std::cin >> str1; // Read the first string from input
-
-    // Prompt the user to enter the second string
+    std::cin >> str1; 
+    
     std::cout << "Enter the second string: ";
-    std::cin >> str2; // Read the second string from input
-
-    // Compare the two strings lexicographically using the less-than operator
+    std::cin >> str2; 
+    
     if (str1 < str2) {
         std::cout << "\"" << str1 << "\" is lexicographically less than \"" << str2 << "\"." << std::endl;
     } else if (str2 < str1) {
@@ -22,5 +18,5 @@ int main() {
         std::cout << "The two strings are lexicographically equal." << std::endl;
     }
 
-    return 0; // Indicate successful program execution
+    return 0; 
 }
