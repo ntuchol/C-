ea451355b5114424d8aa0c89f21e17f965d20f94
Natalia <ntuchol@bu.edu>
2025-09-13 23:02:53 -0500

    Update josephus_simulation.cpp

--- josephus_simulation.cpp
+++ josephus_simulation.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
     #include <vector>
-    #include <numeric> // For std::iota
+    #include <numeric> 
 
     int josephus_simulation(int n, int k) {
         std::vector<int> people(n);
-        std::iota(people.begin(), people.end(), 1); // Fill with 1 to n
+        std::iota(people.begin(), people.end(), 1); 
 
         int current_pos = 0;
         while (people.size() > 1) {
