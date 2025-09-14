1e0a7457b2e6a53d0f485d229b8791a8718d6945
Natalia <ntuchol@bu.edu>
2025-09-13 23:26:06 -0500

    Update nextrand.cpp

--- nextrand.cpp
+++ nextrand.cpp
@@ -1,13 +1,10 @@
-#include <cstdlib> // For rand() and srand()
-    #include <ctime>   // For time()
+#include <cstdlib> 
+#include <ctime>   
 
-    // ... inside main or another function
-    srand(static_cast<unsigned int>(time(nullptr)));
+srand(static_cast<unsigned int>(time(nullptr)));
     
 int next_random_number = rand();
 
-// Generate a random number between 0 and 99 (inclusive)
-    int random_0_to_99 = rand() % 100;
+int random_0_to_99 = rand() % 100;
 
-    // Generate a random number between 1 and 100 (inclusive)
-    int random_1_to_100 = (rand() % 100) + 1;
+int random_1_to_100 = (rand() % 100) + 1;
