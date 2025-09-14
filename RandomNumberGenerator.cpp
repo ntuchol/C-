2ed3df94a4d365ea40eb4b6c7f49c2352e6d0f49
Natalia <ntuchol@bu.edu>
2025-09-13 23:32:17 -0500

    Update RandomNumberGenerator.cpp

--- RandomNumberGenerator.cpp
+++ RandomNumberGenerator.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
-#include <cstdlib> // For rand() and srand()
-#include <ctime>   // For time()
+#include <cstdlib> 
+#include <ctime>   
 
 int main() {
-    // Seed the random number generator
     srand(static_cast<unsigned int>(time(0)));
 
-    // Generate and print a random number
     int randomNumber = rand();
     std::cout << "Random number: " << randomNumber << std::endl;
 
-    // Generate a random number within a specific range (e.g., 1 to 100)
     int min = 1;
     int max = 100;
     int rangedNumber = (rand() % (max - min + 1)) + min;
