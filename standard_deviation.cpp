363310f0b4c227844e37db884e58f9084a2729fa
Natalia <ntuchol@bu.edu>
2025-09-14 16:05:34 -0500

    Update standard_deviation.cpp

--- standard_deviation.cpp
+++ standard_deviation.cpp
@@ -1,5 +1,5 @@
 double sum = 0.0;
-    for (double val : data) { // Assuming 'data' is a container like std::vector<double>
+    for (double val : data) {
         sum += val;
     }
     double mean = sum / data.size();
@@ -8,9 +8,7 @@ double sum = 0.0;
     for (double val : data) {
         variance_sum += pow(val - mean, 2);
     }
-    // For population standard deviation:
     double variance = variance_sum / data.size(); 
-    // For sample standard deviation:
     double variance = variance_sum / (data.size() - 1); 
     double standard_deviation = sqrt(variance);
 
