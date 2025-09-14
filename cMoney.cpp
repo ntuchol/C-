c96cdfc6c9b46c25b2002eb04772f0af1fa3f0a2
Natalia <ntuchol@bu.edu>
2025-09-14 14:18:12 -0500

    Update cMoney.cpp

--- cMoney.cpp
+++ cMoney.cpp
@@ -4,10 +4,9 @@
 #include <string>
 
 int main() {
-    // Set the locale to en_US for demonstration purposes
     std::locale::global(std::locale("en_US.UTF-8"));
 
-    long long cents = 12345; // Represents $123.45
+    long long cents = 12345; 
 
     std::cout << "Amount: " << std::put_money(cents) << std::endl;
 
