ea560f7635ad319eee043568781e10a0703387f6
Natalia <ntuchol@bu.edu>
2025-09-14 00:58:13 -0500

    Update Velocity.cpp

--- Velocity.cpp
+++ Velocity.cpp
@@ -9,13 +9,11 @@ struct Vector2D {
         float z;
     };
 
-// In a game loop or update function
     void updatePosition(Vector3D& position, const Vector3D& velocity, float deltaTime) {
         position.x += velocity.x * deltaTime;
         position.y += velocity.y * deltaTime;
         position.z += velocity.z * deltaTime;
     }
-     // Example of applying acceleration to velocity
     void applyAcceleration(Vector3D& velocity, const Vector3D& acceleration, float deltaTime) {
         velocity.x += acceleration.x * deltaTime;
         velocity.y += acceleration.y * deltaTime;
