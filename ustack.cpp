53adae47e72fbb749f82012d436a5f3561285cbe
Natalia <ntuchol@bu.edu>
2025-09-14 15:33:53 -0500

    Update ustack.cpp

--- ustack.cpp
+++ ustack.cpp
@@ -13,11 +13,10 @@ int main() {
 
     std::cout << "New top element: " << myStack.top() << std::endl; 
 
-    // Check if the stack is empty
     if (myStack.empty()) {
         std::cout << "Stack is empty." << std::endl;
     } else {
-        std::cout << "Stack is not empty. Size: " << myStack.size() << std::endl; // Output: Size: 2
+        std::cout << "Stack is not empty. Size: " << myStack.size() << std::endl; 
     }
 
     return 0;
