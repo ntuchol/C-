fad35bec891a4677fe12e38b8951a8fb478a2c07
Natalia <ntuchol@bu.edu>
2025-09-14 15:31:24 -0500

    Update valuesofproperties.cpp

--- valuesofproperties.cpp
+++ valuesofproperties.cpp
@@ -1,17 +1,14 @@
 class MyClass {
 public:
-    int myIntProperty; // A data member representing an integer property
-    std::string myStringProperty; // A data member representing a string property
+    int myIntProperty; 
+    std::string myStringProperty; 
 
-    // Constructor to initialize properties
     MyClass(int i, const std::string& s) : myIntProperty(i), myStringProperty(s) {}
 
-    // Getter function to access the value of myIntProperty
     int getMyIntProperty() const {
         return myIntProperty;
     }
 };
 
-// Usage:
 MyClass obj(10, "Hello");
-int value = obj.getMyIntProperty(); // 'value' will be 10
+int value = obj.getMyIntProperty(); 
