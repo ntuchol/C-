b8f347fdc65725e2381f680219cb0a60eec72bce
Natalia <ntuchol@bu.edu>
2025-09-13 23:19:34 -0500

    Update min-maxheap.cpp

--- min-maxheap.cpp
+++ min-maxheap.cpp
@@ -1,11 +1,8 @@
  #include <queue>
         #include <vector>
-        #include <functional> // For std::greater
-
-        // Min-heap of integers
+        #include <functional> 
         std::priority_queue<int, std::vector<int>, std::greater<int>> min_pq;
   #include <queue>
   #include <vector>
 
-        // Max-heap of integers (default behavior)
         std::priority_queue<int> max_pq;
