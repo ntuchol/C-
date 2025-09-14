8350a8f0ac52a8de6c479c2bdfee6598a5eb775b
Natalia <ntuchol@bu.edu>
2025-09-14 16:19:17 -0500

    Update Recursive_Function (1).cpp

--- Recursive_Function (1).cpp
+++ Recursive_Function (1).cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 
 int factorial(int n) {
-    // Base Case: Factorial of 0 or 1 is 1
     if (n <= 1) {
         return 1;
     } 
-    // Recursive Case: n * factorial(n-1)
     else {
         return n * factorial(n - 1);
     }
@@ -13,6 +11,6 @@ int factorial(int n) {
 
 int main() {
     int num = 5;
-    std::cout << "Factorial of " << num << " is: " << factorial(num) << std::endl; // Output: Factorial of 5 is: 120
+    std::cout << "Factorial of " << num << " is: " << factorial(num) << std::endl; 
     return 0;
 }
