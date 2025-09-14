c048ad57c503d807a27b6d82ade5721756732bcb
Natalia <ntuchol@bu.edu>
2025-09-14 01:05:27 -0500

    Update Word_Counter.cpp

--- Word_Counter.cpp
+++ Word_Counter.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <string>
-#include <sstream> // Required for std::stringstream
-
+#include <sstream> 
 int main() {
     std::string text = "This is a sample sentence with several words.";
-    std::stringstream ss(text); // Create a stringstream from the text
+    std::stringstream ss(text); 
     std::string word;
     int wordCount = 0;
 
-    // Extract words from the stringstream
     while (ss >> word) {
         wordCount++;
     }
