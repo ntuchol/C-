4e7c8fb2ad09df5e5192c18998b8813a1205c9e1
Natalia <ntuchol@bu.edu>
2025-09-14 15:34:12 -0500

    Update union_integer.cpp

--- union_integer.cpp
+++ union_integer.cpp
@@ -5,9 +5,5 @@ union Data {
     };
 
 Data myData;
-    myData.i = 10; // 'i' now holds 10
-    // At this point, accessing 'f' or 'c' would yield undefined behavior
-    // because the memory is currently interpreted as an integer.
-
-    myData.f = 3.14f; // 'f' now holds 3.14f, overwriting the integer value
-    // Now, accessing 'i' or 'c' would be undefined behavior.
+    myData.i = 10; 
+    myData.f = 3.14f; 
