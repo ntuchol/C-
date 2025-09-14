129c9fda95a1799db2c96493758fa822145cbfad
Natalia <ntuchol@bu.edu>
2025-09-13 19:57:52 -0500

    Update Operator+.cpp

--- Operator+.cpp
+++ Operator+.cpp
@@ -1,7 +1,5 @@
 class MyClass {
     public:
         MyClass operator+(const MyClass& other) const {
-            // Implement addition logic here
-            // Return a new MyClass object representing the sum
         }
     };
