e7c7a664b5508302f16160e6f198ed0d22988a57
Natalia <ntuchol@bu.edu>
2025-09-13 20:06:25 -0500

    Update OutputStream.cpp

--- OutputStream.cpp
+++ OutputStream.cpp
@@ -1,10 +1,7 @@
-#include <iostream> // Required for cout and output streams
-
+#include <iostream> 
 int main() {
-    // Variable initialization
     int myVariable = 42; 
 
-    // Writing the variable's value to the standard output stream (cout)
     std::cout << "The value of myVariable is: " << myVariable << std::endl;
 
     return 0;
