231418901bc170bfd568d4292503d0a4c469d925
Natalia <ntuchol@bu.edu>
2025-09-13 21:07:54 -0500

    Update PowerSet.cpp

--- PowerSet.cpp
+++ PowerSet.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include <vector>
-#include <cmath> // For pow()
+#include <cmath> 
 
 template <typename T>
 void generatePowerSet(const std::vector<T>& set) {
     int n = set.size();
-    // Total number of subsets is 2^n
     int numSubsets = std::pow(2, n);
 
     for (int i = 0; i < numSubsets; ++i) {
         std::cout << "{ ";
         for (int j = 0; j < n; ++j) {
-            // Check if the j-th bit is set in i
             if ((i >> j) & 1) {
                 std::cout << set[j] << " ";
             }
