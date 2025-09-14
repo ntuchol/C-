b9dd759e47be076c74bf60579b8b20d638579aa9
Natalia <ntuchol@bu.edu>
2025-09-14 16:26:00 -0500

    Update StringIterator.cpp

--- StringIterator.cpp
+++ StringIterator.cpp
@@ -4,21 +4,18 @@
 int main() {
     std::string myString = "Hello, C++!";
 
-    // Using a regular iterator to print characters
     std::cout << "Forward iteration: ";
     for (std::string::iterator it = myString.begin(); it != myString.end(); ++it) {
         std::cout << *it;
     }
     std::cout << std::endl;
 
-    // Using a const_iterator (for read-only access)
     std::cout << "Forward const iteration: ";
     for (std::string::const_iterator cit = myString.cbegin(); cit != myString.cend(); ++cit) {
         std::cout << *cit;
     }
     std::cout << std::endl;
 
-    // Using a range-based for loop (simplified iteration)
     std::cout << "Range-based for loop: ";
     for (char c : myString) {
         std::cout << c;
