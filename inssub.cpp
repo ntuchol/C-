a3b174f478984319da5a4868a6a1d4978a3aa813
Natalia <ntuchol@bu.edu>
2025-09-13 21:39:25 -0500

    Update inssub.cpp

--- inssub.cpp
+++ inssub.cpp
@@ -3,8 +3,8 @@
 
 int main() {
     std::string str = "Hello World";
-    std::string sub = str.substr(6, 5); // Extracts 5 characters starting from index 6
-    std::cout << sub << std::endl; // Output: World
+    std::string sub = str.substr(6, 5); 
+    std::cout << sub << std::endl; 
     return 0;
 }
 
@@ -14,7 +14,7 @@ int main() {
 int main() {
     std::string str1 = "Hello World!";
     std::string str2 = "Amazing ";
-    str1.insert(6, str2); // Inserts "Amazing " at index 6 in str1
-    std::cout << str1; // Output: Hello Amazing World!
+    str1.insert(6, str2); 
+    std::cout << str1; 
     return 0;
 }
