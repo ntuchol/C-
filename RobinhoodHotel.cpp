c6beaff842e819e671baa3eb89ce8937ad5efa39
Natalia <ntuchol@bu.edu>
2025-09-14 16:26:52 -0500

    Update RobinhoodHotel.cpp

--- RobinhoodHotel.cpp
+++ RobinhoodHotel.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
-#include "robin_hood.h" // Assuming you're using the martinus/robin-hood-hashing library
+#include "robin_hood.h" 
 
 struct RoomInfo {
     std::string roomType;
     bool isBooked;
-    // other room details
 };
 
 int main() {
     robin_hood::unordered_map<int, RoomInfo> hotelRooms;
 
-    // Insert room information
     hotelRooms[101] = {"Single", false};
     hotelRooms[102] = {"Double", true};
 
-    // Check availability
     if (!hotelRooms[101].isBooked) {
-        // Book room 101
         hotelRooms[101].isBooked = true;
         std::cout << "Room 101 booked." << std::endl;
     } else {
