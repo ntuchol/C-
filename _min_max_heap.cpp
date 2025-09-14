f9cadee1fbb9befb68598203baecf92551a87a23
Natalia <ntuchol@bu.edu>
2025-09-14 01:11:40 -0500

    Update _min_max_heap.cpp

--- _min_max_heap.cpp
+++ _min_max_heap.cpp
@@ -1,10 +1,10 @@
 #include <queue>
 #include <vector>
 
-        std::priority_queue<int> max_heap; // Default: max-heap
+        std::priority_queue<int> max_heap; 
         
 #include <queue>
 #include <vector>
-#include <functional> // For std::greater
+#include <functional> 
 
-        std::priority_queue<int, std::vector<int>, std::greater<int>> min_heap; // Min-heap
+        std::priority_queue<int, std::vector<int>, std::greater<int>> min_heap; 
