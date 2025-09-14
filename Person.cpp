7470a9d144a14aebafe6cce869e16881809a08b3
Natalia <ntuchol@bu.edu>
2025-09-13 20:52:28 -0500

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,4 +1,4 @@
-#include <algorithm> // For std::sort
+#include <algorithm> 
 #include <vector>
 #include <iostream>
 
@@ -10,8 +10,6 @@ struct Person {
 int main() {
     std::vector<Person> people = {{"Alice", 30}, {"Bob", 25}, {"Charlie", 30}};
 
-    // Sort people by age in ascending order
-    // If ages are equal, sort by name alphabetically
     std::sort(people.begin(), people.end(), [](const Person& p1, const Person& p2) {
         if (p1.age != p2.age) {
             return p1.age < p2.age;
