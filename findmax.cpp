6be444e2f43629a3eafc74b7ea16dbd5df592c43
Natalia <ntuchol@bu.edu>
2025-09-14 15:01:09 -0500

    Update findmax.cpp

--- findmax.cpp
+++ findmax.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <vector>
-#include <algorithm> // Required for std::max_element
-
+#include <algorithm> 
 int main() {
     std::vector<int> numbers = {5, 12, 3, 8, 20, 1};
     
-    // Find the iterator to the maximum element
     auto max_it = std::max_element(numbers.begin(), numbers.end());
     
-    // Dereference the iterator to get the value
     int max_val = *max_it;
     
     std::cout << "The maximum element in the vector is: " << max_val << std::endl;
