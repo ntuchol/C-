0baa94e1a08cf053bb780388140581d2d8b5ddea
Natalia <ntuchol@bu.edu>
2025-09-14 16:49:04 -0500

    Update Shell_Sort (1).cpp

--- Shell_Sort (1).cpp
+++ Shell_Sort (1).cpp
@@ -4,19 +4,14 @@
 void shellSort(std::vector<int>& arr) {
     int n = arr.size();
 
-    // Start with a large gap and reduce it
     for (int gap = n / 2; gap > 0; gap /= 2) {
-        // Perform a gapped insertion sort for the current gap
         for (int i = gap; i < n; i++) {
             int temp = arr[i];
             int j;
 
-            // Shift earlier gap-sorted elements up until the correct
-            // location for arr[i] is found
             for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
                 arr[j] = arr[j - gap];
             }
-            // Put temp (the original arr[i]) in its correct location
             arr[j] = temp;
         }
     }
