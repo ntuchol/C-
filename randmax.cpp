b7335a00518cf506070979f81ee7ad00b1633ed6
Natalia <ntuchol@bu.edu>
2025-09-14 15:46:41 -0500

    Update randmax.cpp

--- randmax.cpp
+++ randmax.cpp
@@ -1,6 +1,5 @@
 #include <cstdlib> 
     #include <iostream>
-
     int main() {
         double random_double = static_cast<double>(rand()) 
         std::cout << "Random double: " << random_double << std::endl;
