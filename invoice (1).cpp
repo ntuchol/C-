be97b114d0f945213444e1e4b9ad09d86b63407d
Natalia <ntuchol@bu.edu>
2025-09-13 21:45:24 -0500

    Update invoice (1).cpp

--- invoice (1).cpp
+++ invoice (1).cpp
@@ -2,22 +2,18 @@
 
 class Invoice {
 public:
-    // Constructor
     Invoice(std::string pNum, std::string pDesc, int qty, double price);
 
-    // Mutator functions with validation
     void setPartNumber(std::string pNum);
     void setPartDescription(std::string pDesc);
     void setQuantity(int qty);
     void setPricePerItem(double price);
 
-    // Accessor functions
     std::string getPartNumber() const;
     std::string getPartDescription() const;
     int getQuantity() const;
     double getPricePerItem() const;
 
-    // Calculate invoice amount
     double getInvoiceAmount() const;
 
 private:
