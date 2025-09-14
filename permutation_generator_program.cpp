7fdffca9aadc74c669eedf82902a488a9faa7bbc
Natalia <ntuchol@bu.edu>
2025-09-14 00:23:45 -0500

    Update permutation_generator_program.cpp

--- permutation_generator_program.cpp
+++ permutation_generator_program.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
 #include <string>
-#include <algorithm> // Required for std::next_permutation
-
+#include <algorithm> 
 int main() {
-    std::string s = "abc"; // The string for which to generate permutations
-
-    // Sort the string initially to get the first lexicographical permutation
-    // std::next_permutation works correctly only if the range is sorted initially
+    std::string s = "abc"; 
     std::sort(s.begin(), s.end());
 
     std::cout << "Permutations of \"" << s << "\":" << std::endl;
 
-    // Loop to generate and print all permutations
     do {
         std::cout << s << std::endl;
     } while (std::next_permutation(s.begin(), s.end()));
