355a5046b46168c7653621fca3ed6bd35abdafd9
Natalia <ntuchol@bu.edu>
2025-09-14 16:40:09 -0500

    Update State_Abbreviations.cpp

--- State_Abbreviations.cpp
+++ State_Abbreviations.cpp
@@ -3,17 +3,14 @@
 #include <map>
 
 int main() {
-    // Create a map to store state name to abbreviation pairs
     std::map<std::string, std::string> stateAbbreviations;
 
-    // Populate the map with some state abbreviations
     stateAbbreviations["Alabama"] = "AL";
     stateAbbreviations["Alaska"] = "AK";
     stateAbbreviations["Arizona"] = "AZ";
     stateAbbreviations["California"] = "CA";
     stateAbbreviations["New York"] = "NY";
 
-    // Example of retrieving an abbreviation
     std::string stateName = "California";
     auto it = stateAbbreviations.find(stateName);
 
@@ -23,7 +20,6 @@ int main() {
         std::cout << "Abbreviation not found for " << stateName << std::endl;
     }
 
-    // Example of handling a state not in the map
     stateName = "Florida";
     it = stateAbbreviations.find(stateName);
     if (it != stateAbbreviations.end()) {
