fc84325befe283c05af6023d01b0598ac0ba432c
Natalia <ntuchol@bu.edu>
2025-09-13 21:38:49 -0500

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -4,16 +4,14 @@
 void insertionSort(std::vector<int>& arr) {
     int n = arr.size();
     for (int i = 1; i < n; ++i) {
-        int key = arr[i]; // Element to be inserted
-        int j = i - 1;    // Index of the last element in the sorted part
-
-        // Move elements of arr[0..i-1] that are greater than key
-        // to one position ahead of their current position
+        int key = arr[i]; 
+        int j = i - 1;    
+        
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
         }
-        arr[j + 1] = key; // Insert the key at its correct position
+        arr[j + 1] = key; 
     }
 }
 
