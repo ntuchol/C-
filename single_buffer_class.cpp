739a41e84620f345b2b77c23a54656398053df2c
Natalia <ntuchol@bu.edu>
2025-09-14 16:02:30 -0500

    Update single_buffer_class.cpp

--- single_buffer_class.cpp
+++ single_buffer_class.cpp
@@ -1,21 +1,18 @@
-#include <cstddef> // For size_t
-#include <algorithm> // For std::copy
+#include <cstddef> 
+#include <algorithm> 
 
 class SingleBuffer {
 public:
-    // Constructor: Allocates memory for the buffer
     explicit SingleBuffer(size_t size) : m_size(size), m_buffer(nullptr) {
         if (m_size > 0) {
             m_buffer = new char[m_size];
         }
     }
 
-    // Destructor: Deallocates memory
     ~SingleBuffer() {
         delete[] m_buffer;
     }
 
-    // Copy Constructor
     SingleBuffer(const SingleBuffer& other) : m_size(other.m_size), m_buffer(nullptr) {
         if (m_size > 0) {
             m_buffer = new char[m_size];
@@ -23,10 +20,9 @@ public:
         }
     }
 
-    // Copy Assignment Operator
     SingleBuffer& operator=(const SingleBuffer& other) {
-        if (this != &other) { // Handle self-assignment
-            delete[] m_buffer; // Deallocate old memory
+        if (this != &other) { 
+            delete[] m_buffer; 
             m_size = other.m_size;
             m_buffer = nullptr;
             if (m_size > 0) {
@@ -37,14 +33,12 @@ public:
         return *this;
     }
 
-    // Move Constructor (C++11 and later)
     SingleBuffer(SingleBuffer&& other) noexcept
         : m_size(other.m_size), m_buffer(other.m_buffer) {
         other.m_size = 0;
         other.m_buffer = nullptr;
     }
 
-    // Move Assignment Operator (C++11 and later)
     SingleBuffer& operator=(SingleBuffer&& other) noexcept {
         if (this != &other) {
             delete[] m_buffer;
@@ -56,7 +50,6 @@ public:
         return *this;
     }
 
-    // Accessors
     char* data() {
         return m_buffer;
     }
