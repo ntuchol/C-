30c408670a07ee262d456c37b9988f52ee82e3ae
Natalia <ntuchol@bu.edu>
2025-09-13 19:55:16 -0500

    Update RandomAccessStream.cpp

--- RandomAccessStream.cpp
+++ RandomAccessStream.cpp
@@ -9,28 +9,23 @@ int main() {
         return 1;
     }
 
-    // Write some data
     file.write("Hello", 5);
     file.write("World", 5);
 
-    // Move the get pointer to the beginning
     file.seekg(0, std::ios::beg);
     char buffer[6];
     file.read(buffer, 5);
     buffer[5] = '\0';
     std::cout << "Read from beginning: " << buffer << std::endl;
 
-    // Move the get pointer to a specific offset (e.g., after "Hello")
     file.seekg(5, std::ios::beg);
     file.read(buffer, 5);
     buffer[5] = '\0';
     std::cout << "Read from offset 5: " << buffer << std::endl;
 
-    // Move the put pointer to overwrite "World" with "There"
     file.seekp(5, std::ios::beg);
     file.write("There", 5);
 
-    // Read the modified content from the beginning
     file.seekg(0, std::ios::beg);
     char full_buffer[11];
     file.read(full_buffer, 10);
