e41e5be679b63073a7d6c2f368872e5784fb31dd
Natalia <ntuchol@bu.edu>
2025-09-13 23:24:11 -0500

    Update n-vertex.cpp

--- n-vertex.cpp
+++ n-vertex.cpp
@@ -1,9 +1,5 @@
-// Example of an adjacency list for n vertices
         std::vector<std::list<int>> adj;
-        // In the constructor:
-        // adj.resize(n); 
+        adj.resize(n); 
         
-   // Example of an adjacency matrix for n vertices
         std::vector<std::vector<bool>> adj_matrix;
-        // In the constructor:
-        // adj_matrix.resize(n, std::vector<bool>(n, false));
+        adj_matrix.resize(n, std::vector<bool>(n, false));
