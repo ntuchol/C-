35ee7e752f90ff402b4160013b3c25717b317f4f
Natalia <ntuchol@bu.edu>
2025-09-14 11:40:33 -0500

    Update qNode.cpp

--- qNode.cpp
+++ qNode.cpp
@@ -1,9 +1,6 @@
 #include <Qt3DCore/QNode>
 #include <Qt3DCore/QEntity>
 
-// ...
 
 Qt3DCore::QEntity* rootEntity = new Qt3DCore::QEntity();
-Qt3DCore::QEntity* childEntity = new Qt3DCore::QEntity(rootEntity); // childEntity has rootEntity as its parent QNode
-
-// ...
+Qt3DCore::QEntity* childEntity = new Qt3DCore::QEntity(rootEntity); 
