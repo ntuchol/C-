14fb3dae8cef3db6bdd8295ceccf4390f6740866
Natalia <ntuchol@bu.edu>
2025-09-13 20:15:56 -0500

    Update Pascal_Program.cpp

--- Pascal_Program.cpp
+++ Pascal_Program.cpp
@@ -1,11 +1,10 @@
-// C++ code
 extern "C" {
-  void PascalProcedure(int *data, int size); // Declaration of Pascal procedure
+  void PascalProcedure(int *data, int size); 
 }
 
 int main() {
   int data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  PascalProcedure(data, 10); // Calling the Pascal procedure
+  PascalProcedure(data, 10); 
   return 0;
 }
 
