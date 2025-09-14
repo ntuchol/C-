cdf9254aa352352896c761b7aa905388f4bb519c
Natalia <ntuchol@bu.edu>
2025-09-13 23:16:46 -0500

    Update maximumlengthof1000.cpp

--- maximumlengthof1000.cpp
+++ maximumlengthof1000.cpp
@@ -3,4 +3,4 @@ memset(c_style_string, '\0', sizeof(c_style_string));
 std::cin.getline(c_style_string, sizeof(c_style_string));
 const char* source_string = "Hello, world!";
 strncpy(c_style_string, source_string, sizeof(c_style_string) - 1);
-c_style_string[sizeof(c_style_string) - 1] = '\0'; // Ensure null termination
+c_style_string[sizeof(c_style_string) - 1] = '\0';
