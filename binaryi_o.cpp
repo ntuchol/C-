ea3220c302de1c552e82fae29a23638e7adb4337
Natalia <ntuchol@bu.edu>
2025-09-14 01:36:20 -0500

    Update binaryi_o.cpp

--- binaryi_o.cpp
+++ binaryi_o.cpp
@@ -1,12 +1,9 @@
 #include <fstream>
 
-    // For writing
     std::ofstream outFile("data.bin", std::ios::out | std::ios::binary);
 
-    // For reading
     std::ifstream inFile("data.bin", std::ios::in | std::ios::binary);
 
-    // For read/write
     std::fstream rwFile("data.bin", std::ios::in | std::ios::out | std::ios::binary);
 
     int value = 123;
