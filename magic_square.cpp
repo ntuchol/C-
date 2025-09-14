0195ddf5beebd03d4e2a1ed4a61d7cd7f86b0f5c
Natalia <ntuchol@bu.edu>
2025-09-13 23:16:21 -0500

    Update magic_square.cpp

--- magic_square.cpp
+++ magic_square.cpp
@@ -9,21 +9,21 @@
 
         std::vector<std::vector<int>> magicSquare(n, std::vector<int>(n, 0));
 
-        int i = n / 2; // Starting row (middle of top row)
-        int j = n - 1; // Starting column (last column)
-
+        int i = n / 2; 
+        int j = n - 1; 
+        
         for (int num = 1; num <= n * n; ) {
-            if (i == -1 && j == n) { // Wrap around from top-right corner
+            if (i == -1 && j == n) { 
                 j = n - 2;
                 i = 0;
             } else {
-                if (j == n) // Wrap around column
+                if (j == n) 
                     j = 0;
-                if (i < 0) // Wrap around row
+                if (i < 0) 
                     i = n - 1;
             }
 
-            if (magicSquare[i][j] != 0) { // If cell is already filled
+            if (magicSquare[i][j] != 0) { 
                 j -= 2;
                 i++;
                 continue;
@@ -31,10 +31,9 @@
                 magicSquare[i][j] = num++;
             }
 
-            i--; j++; // Move diagonally up-right
+            i--; j++; 
         }
 
-        // Print the magic square
         for (int row = 0; row < n; row++) {
             for (int col = 0; col < n; col++) {
                 std::cout << magicSquare[row][col] << "\t";
