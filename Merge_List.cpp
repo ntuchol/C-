2d89a41b1de1524918de4ffb9fdb1ac87b3322f6
Natalia <ntuchol@bu.edu>
2025-09-13 19:31:55 -0500

    Update Merge_List.cpp

--- Merge_List.cpp
+++ Merge_List.cpp
@@ -1,12 +1,12 @@
 #include <list>
 #include <iostream>
-#include <algorithm> // For std::for_each
+#include <algorithm> 
 
 int main() {
     std::list<int> list1 = {10, 30, 50};
     std::list<int> list2 = {20, 40, 60};
 
-    list1.merge(list2); // Merges list2 into list1
+    list1.merge(list2); 
 
     std::cout << "Merged list: ";
     std::for_each(list1.begin(), list1.end(), [](int n){ std::cout << n << " "; });
@@ -22,8 +22,8 @@ int main() {
     std::list<int> listA = {1, 2, 3};
     std::list<int> listB = {4, 5, 6};
 
-    listA.splice(listA.end(), listB); // Appends listB to the end of listA
-
+    listA.splice(listA.end(), listB); 
+    
     std::cout << "Concatenated list: ";
     for (int n : listA) {
         std::cout << n << " ";
