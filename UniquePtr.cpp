0e496406063a8854ac41e50dcb4a9a3ce0c5bd16
Natalia <ntuchol@bu.edu>
2025-09-14 00:55:16 -0500

    Update UniquePtr.cpp

--- UniquePtr.cpp
+++ UniquePtr.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <memory> // Required for std::unique_ptr
-
+#include <memory> 
 class MyClass {
 public:
     MyClass() { std::cout << "MyClass created\n"; }
@@ -8,17 +7,13 @@ public:
 };
 
 int main() {
-    // Create a unique_ptr owning a MyClass object
     std::unique_ptr<MyClass> ptr1 = std::make_unique<MyClass>();
 
-    // Transfer ownership to another unique_ptr
     std::unique_ptr<MyClass> ptr2 = std::move(ptr1);
 
-    // ptr1 is now empty, ptr2 owns the object
     if (!ptr1) {
         std::cout << "ptr1 is empty\n";
     }
 
-    // The object will be destroyed when ptr2 goes out of scope
     return 0;
 }
