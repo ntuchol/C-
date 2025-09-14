a7fbc13422db5f86b393430e9a15266cb777c07e
Natalia <ntuchol@bu.edu>
2025-09-14 11:37:19 -0500

    Update putQueen.cpp

--- putQueen.cpp
+++ putQueen.cpp
@@ -1,17 +1,17 @@
 bool putQueen(int board[][N], int row) {
-    if (row >= N) { // Base case: All queens placed
-        printSolution(board); // Print the solution
+    if (row >= N) { 
+        printSolution(board); 
         return true; 
     }
 
     for (int col = 0; col < N; col++) {
         if (isSafe(board, row, col)) {
-            board[row][col] = 1; // Place the queen
-            if (putQueen(board, row + 1)) { // Recurse for the next row
+            board[row][col] = 1; 
+            if (putQueen(board, row + 1)) { 
                 return true; 
             }
-            board[row][col] = 0; // Backtrack: Remove the queen
+            board[row][col] = 0; 
         }
     }
-    return false; // No solution found for this branch
+    return false; 
 }
