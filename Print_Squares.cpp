e534107f16222061bd38e2758a766777e8c43cba
Natalia <ntuchol@bu.edu>
2025-09-13 21:13:02 -0500

    Update Print_Squares.cpp

--- Print_Squares.cpp
+++ Print_Squares.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
     int main() {
-        // Print squares of numbers from 1 to 10
         for (int i = 1; i <= 10; ++i) {
             int square = i * i;
             std::cout << "The square of " << i << " is " << square << std::endl;
