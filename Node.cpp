707efe1d2fb5a118b83ad22600fa469fe8514793
Natalia <ntuchol@bu.edu>
2025-09-13 19:53:27 -0500

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,9 +1,7 @@
 template <typename T>
 class Node {
 public:
-    T data; // Data stored in the node
-    Node<T>* next; // Pointer to the next node in a sequence (e.g., linked list)
-
-    // Constructor
+    T data; 
+    Node<T>* next; 
     Node(T val) : data(val), next(nullptr) {}
 };
